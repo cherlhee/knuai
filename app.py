f77066985f4b2161932db8df4f8ed6799151b406
cherlhee <52117338+cherlhee@users.noreply.github.com>
2025-12-01 15:44:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -22,32 +22,3 @@ if uploaded:
 
     cap = cv2.VideoCapture(temp.name)
 
-    with mp_pose.Pose(
-        static_image_mode=False,
-        min_detection_confidence=0.5,
-        min_tracking_confidence=0.5
-    ) as pose:
-
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
-                break
-
-            # BGR → RGB
-            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-            # Mediapipe Pose detect
-            results = pose.process(image)
-
-            # Draw skeleton
-            if results.pose_landmarks:
-                mp_drawing.draw_landmarks(
-                    image,
-                    results.pose_landmarks,
-                    mp_pose.POSE_CONNECTIONS
-                )
-
-            # Display in Streamlit
-            stframe.image(image, channels="RGB")
-
-    cap.release()
